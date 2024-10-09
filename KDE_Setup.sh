f551d2107c841dee66e827edd3ca3cce3ef2a110
Jason Soar <41485578+JDSoar@users.noreply.github.com>
2024-10-09 10:57:32 -0400

    Update KDE_Setup.sh
    Removes Gnome packages and sets KDE to Default Login display Manager

--- KDE_Setup.sh
+++ KDE_Setup.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
+echo "WARNING: Running this script will reboot your system!"
+
 
 #Set Wallpaper
-
 # Get the full path of the script's directory
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
@@ -20,5 +21,32 @@ qdbus org.kde.plasmashell /PlasmaShell org.kde.PlasmaShell.evaluateScript '
 echo "Wallpaper has been set to $SCRIPT_DIR/hacker.jpg"
 
 
-#change to darkmode
+#Change Desktop to Darkmode
 lookandfeeltool -a org.kde.breezedark.desktop
+
+
+#Remove Gnome Desktop and Packages
+sudo apt purge ubuntu-desktop gnome-shell
+sudo apt purge gnome*
+sudo apt autoremove --purge
+
+#Set KDE as the Default Login Display Manager
+sudo apt install sddm
+sudo apt install -y debconf-utils
+echo "sddm shared/default-x-display-manager select sddm" | sudo debconf-set-selections
+
+#Minimize the on-screen Keyboard on Reboot
+
+
+#Set Login Screen Background
+sudo mkdir -p /etc/sddm.conf.d
+sudo tee /etc/sddm.conf.d/kde_settings.conf > /dev/null <<EOL
+[Theme]
+Current=breezedark
+
+[Greeter]
+Background="file://'"$SCRIPT_DIR"'/hacker.jpg"
+EOL
+
+
+sudo reboot
