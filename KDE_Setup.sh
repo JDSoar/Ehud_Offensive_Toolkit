403ee8805eeab7db407abc76a85e7937f0ab7213
Jason Soar <41485578+JDSoar@users.noreply.github.com>
2024-10-09 21:47:54 -0400

    Update KDE_Setup.sh
    Disable Ubuntu Splash Screen

--- KDE_Setup.sh
+++ KDE_Setup.sh
@@ -34,15 +34,21 @@ sudo apt autoremove --purge
 sudo apt install sddm
 sudo apt install -y debconf-utils
 echo "sddm shared/default-x-display-manager select sddm" | sudo debconf-set-selections
-
 #Minimize the on-screen Keyboard on Reboot
 sudo bash -c 'echo "InputMethod=" > /etc/sddm.conf'
 
 #Set Login Screen Background
 # Get the full path of the script's directory
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-
 kwriteconfig5 --file kscreenlockerrc --group Greeter --group Wallpaper --group org.kde.image --group General --key Image "file://$SCRIPT_DIR/hacker.jpg"
 
+#Disable Ubuntu Splash Screen
+# Backup the current GRUB configuration
+sudo cp /etc/default/grub /etc/default/grub.bak
+# Disable splash screen by modifying GRUB configuration
+sudo sed -i 's/GRUB_CMDLINE_LINUX_DEFAULT="quiet splash"/GRUB_CMDLINE_LINUX_DEFAULT=""/' /etc/default/grub
+# Update GRUB
+sudo update-grub
+echo "Splash screen disabled. Please reboot to see the changes."
 
 sudo reboot
